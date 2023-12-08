8899e605633bdb43b6e55e3f16f725a087066c1c
N4h33n <naheenkabir@gmail.com>
2023-12-08 00:56:05 -0700

    more queries

--- centralhub/backend/app/routes.py
+++ centralhub/backend/app/routes.py
@@ -15,7 +15,7 @@ def get_db_connection():
     
 # testing using dummy template test.html will change later
 def create_routes(app):
-    
+
     @app.route('/api/adminlogin', methods = ['POST'])
     @cross_origin(origin=host_url, headers=['Content-Type', 'Authorization'])
     def admin_login():
@@ -23,16 +23,16 @@ def create_routes(app):
         email = data.get('email')
         password = data.get('password')
         try:
-            # Establish a database connection
+
             connection = get_db_connection()
 
-            # Create a cursor
+
             cursor = connection.cursor()
 
-            # Execute a test query
+
             query = f"SELECT * from admin as A, faculty as F Where F.f_ucid = A.a_ucid and F.email = '{email}' and A.passhash = '{password}'"
             cursor.execute(query)
-            # Fetch the result
+
             result = cursor.fetchone()
 
             if result:
@@ -40,12 +40,7 @@ def create_routes(app):
         
             else:
                 return jsonify({'success': False})
-
             
-            # Close the cursor and connection
-            cursor.close()
-            connection.close()
-
         except mysql.connector.Error as e:
                 print(f"Error: {e}")
         
@@ -105,5 +100,218 @@ def create_routes(app):
         finally:
             cursor.close()
             connection.close()
+    
+    @app.route('/api/studentinfo', methods = ['POST'])
+    @cross_origin(origin=host_url, headers=['Content-Type', 'Authorization'])
+    def student_info():
+        data = request.get_json()
+        
+        try:
+            connection = get_db_connection()
+
+            cursor = connection.cursor()
+
+            query = "SELECT C.courseno, C.coursename, C.semester from COURSE as C, STUDENT_ENROLLEDIN_COURSE as SC where SC.s_ucid = (%s) and SC.courseno = C.courseno"
+            values = (data.get("ucid"))
+            cursor.execute(query, values)
+
+            columns = [column[0] for column in cursor.description]
+            result = [dict(zip(columns, row)) for row in cursor.fetchall()]
+
+            return jsonify(result)
+        
+        except mysql.connector.Error as e:
+            print(f"Error{e}")
+        
+        finally:
+            cursor.close()
+            connection.close()
+            
+    #POST?   
+    @app.route('/api/studentlogin', methods = ['POST'])
+    @cross_origin(origin=host_url, headers=['Content-Type', 'Authorization'])
+    def student_info():
+        data = request.get_json()
+        
+        try:
+            connection = get_db_connection()
+
+            cursor = connection.cursor()
+
+            query = "SELECT * from STUDENT as S where S.email = (%s) and S.passhash = (%s)"
+            values = (data.get("email"), data.get("password"))
+            cursor.execute(query, values)
+
+            
+            result = cursor.fetchone()
+
+            if result:
+                return jsonify({'success': True})
+        
+            else:
+                return jsonify({'success': False})
+        
+        except mysql.connector.Error as e:
+            print(f"Error{e}")
+        
+        finally:
+            cursor.close()
+            connection.close()
+              
+    @app.route('/api/dashboardassignments', methods = ['POST'])
+    @cross_origin(origin=host_url, headers=['Content-Type', 'Authorization'])
+    def dashboard_assignments():
+        data = request.get_json()
+        
+        try:
+            connection = get_db_connection()
+
+            cursor = connection.cursor()
+
+            query = "SELECT SA.courseno, SA.assignmentno, A.deadline, A.weight from STUDENT_DOES_ASSIGNMENT as SA, ASSIGNMENT as A where SA.s_ucid = (%s) and SA.courseno = A.courseno and SA.assignmentno = A.assignmentno"
+            values = (data.get("ucid"))
+            cursor.execute(query, values)
+
+            columns = [column[0] for column in cursor.description]
+            result = [dict(zip(columns, row)) for row in cursor.fetchall()]
+
+            return jsonify(result)
+        
+        except mysql.connector.Error as e:
+            print(f"Error{e}")
+        
+        finally:
+            cursor.close()
+            connection.close()
+            
+    @app.route('/api/dashboardexams', methods = ['POST'])
+    @cross_origin(origin=host_url, headers=['Content-Type', 'Authorization'])
+    def dashboard_exams():
+        data = request.get_json()
+        
+        try:
+            connection = get_db_connection()
+
+            cursor = connection.cursor()
+
+            query = "SELECT SE.courseno, SE.examno, E.time, E.location, E.weight from STUDENT_TAKES_EXAM as SE, EXAM as E where SE.s_ucid = (%s) and SE.courseno = E.courseno and SE.examno = E.examno"
+            values = (data.get("ucid"))
+            cursor.execute(query, values)
+
+            columns = [column[0] for column in cursor.description]
+            result = [dict(zip(columns, row)) for row in cursor.fetchall()]
+
+            return jsonify(result)
+        
+        except mysql.connector.Error as e:
+            print(f"Error{e}")
+        
+        finally:
+            cursor.close()
+            connection.close()
+            
+    @app.route('/api/dashboardname', methods = ['POST'])
+    @cross_origin(origin=host_url, headers=['Content-Type', 'Authorization'])
+    def dashboard_name():
+        data = request.get_json()
+        
+        try:
+            connection = get_db_connection()
+
+            cursor = connection.cursor()
+
+            query = "SELECT S.name from STUDENT as S where S.s_ucid = (%s)"
+            values = (data.get("ucid"))
+            cursor.execute(query, values)
+
+            columns = [column[0] for column in cursor.description]
+            result = [dict(zip(columns, row)) for row in cursor.fetchall()]
+
+            return jsonify(result)
+        
+        except mysql.connector.Error as e:
+            print(f"Error{e}")
+        
+        finally:
+            cursor.close()
+            connection.close()
+
+    @app.route('/api/studentinfodefault', methods = ['POST'])
+    @cross_origin(origin=host_url, headers=['Content-Type', 'Authorization'])
+    def student_info_default():
+        data = request.get_json()
+        
+        try:
+            connection = get_db_connection()
+
+            cursor = connection.cursor()
+
+            query = "SELECT S.name, S.email, S.s_ucid, s.address, s.passhash from STUDENT as S where S.s_ucid = (%s)"
+            values = (data.get("ucid"))
+            cursor.execute(query, values)
+
+            columns = [column[0] for column in cursor.description]
+            result = [dict(zip(columns, row)) for row in cursor.fetchall()]
+
+            return jsonify(result)
+        
+        except mysql.connector.Error as e:
+            print(f"Error{e}")
+        
+        finally:
+            cursor.close()
+            connection.close()
+            
+    # @app.route('/api/enrolledcourses', methods = ['POST'])
+    # @cross_origin(origin=host_url, headers=['Content-Type', 'Authorization'])
+    # def enrolled_courses():
+    #     data = request.get_json()
+        
+    #     try:
+    #         connection = get_db_connection()
+
+    #         cursor = connection.cursor()
+
+    #         query = "select sc.courseno, c.coursename, (sum(sa.grade * a.weight) + sum(se.grade * e.weight)) from COURSE as c, STUDENT_ENROLLEDIN_COURSE as sc, STUDENT_DOES_ASSIGNMENT as sa. STUDENT_TAKES_EXAM as se, ASSIGNMENT as a, EXAM as e where sc"
+    #         values = (data.get("telephonenumber"), data.get("address"), data.get("password"), data.get("ucid"))
+    #         cursor.execute(query, values)
+
+    #         connection.commit()
+                
+    #         return "True"
+            
+    #     except mysql.connector.Error as e:
+    #             print(f"Error: {e}")
+
+    #             return "False"
+    #     finally:
+    #         cursor.close()
+    #         connection.close()
+    
+    @app.route('/api/explorecourses', methods = ['GET'])
+    @cross_origin(origin=host_url, headers=['Content-Type', 'Authorization'])
+    def explore_courses():
+        
+        try:
+            connection = get_db_connection()
+
+            cursor = connection.cursor()
+
+            query = "SELECT c.courseno, c.coursename, c.semester, l.lectureno, f1.name, t.tutorialno, f2.name, cf.field from COURSE as c, LECTURE as l, TUTORIAL as t, FACULTY as f1, FACULTY as f2 where l.courseno = c.courseno and t.courseno = c.courseno and l.i_ucid = f1.f_ucid and t.t_ucid = f2.f_ucid and cf.courseno = c.courseno"
+            cursor.execute(query)
+
+            columns = [column[0] for column in cursor.description]
+            result = [dict(zip(columns, row)) for row in cursor.fetchall()]
+
+            return jsonify(result)
+        
+        except mysql.connector.Error as e:
+            print(f"Error{e}")
+        
+        finally:
+            cursor.close()
+            connection.close()
+            
+        
         
 
