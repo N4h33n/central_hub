d2032fba8d2fb7958cb939443c4b7d6d37035198
Abigia Debebe <Abitibebu123@gmail.com>
2023-12-11 17:15:35 -0700

    ...

--- centralhub/src/Pages (student)/ECA/DiscoverECA.js
+++ centralhub/src/Pages (student)/ECA/DiscoverECA.js
@@ -33,6 +33,7 @@ export default function DiscoverECA() {
       const ecas = await response.json();
       console.log("ecas", ecas);
       setData(ecas);
+      console.log(data);
     } catch (error) {
       // console.error("Error loading courses:", error);
     }
