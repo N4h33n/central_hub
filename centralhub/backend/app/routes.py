a5ebd46143eaff233c6e460b647f3c9ba5e46ca6
N4h33n <naheenkabir@gmail.com>
2023-12-12 20:45:40 -0700

    eca works

--- centralhub/backend/app/routes.py
+++ centralhub/backend/app/routes.py
@@ -8,13 +8,13 @@ from werkzeug.security import generate_password_hash, check_password_hash
 # reference for using parametrized queries (query, values) to prevent sql injections in flask: https://www.reddit.com/r/flask/comments/zr9148/question_about_protecting_against_sql_injections/
 
 routes = Blueprint('routes', __name__)
-host_url = 'http://localhost:3003'
+host_url = 'http://localhost:3000'
 
 def get_db_connection():
     return mysql.connector.connect(
         host='localhost',
         user='root',
-        password="*PASSworld*123",
+        password="sQlprequelwoohoo7676",
         database='centralhub'
     )
     
@@ -556,7 +556,7 @@ def create_routes(app):
 
             # reference for not duplicating rows for multivalued attribute field using group_concat: https://stackoverflow.com/questions/12095450/how-to-put-a-multivalued-attribute-in-one-column-in-a-query
             query = "SELECT c.clubname, group_concat(cf.field separator ', ') as fields, c.location, c.time, c.description from CLUB as c, CLUB_FIELDS as cf where cf.clubname = c.clubname group by c.clubname, c.location, c.time, c.description"
-            cursor.execute(query, values)
+            cursor.execute(query)
 
             columns = [column[0] for column in cursor.description]
             result = [dict(zip(columns, row)) for row in cursor.fetchall()]
