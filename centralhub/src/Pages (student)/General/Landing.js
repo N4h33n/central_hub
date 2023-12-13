8aeeaffb43597ca00401dcf0c572ec29bdb7205d
Abigia Debebe <Abitibebu123@gmail.com>
2023-12-12 23:03:46 -0700

    ...

--- centralhub/src/Pages (student)/General/Landing.js
+++ centralhub/src/Pages (student)/General/Landing.js
@@ -2,8 +2,15 @@ import React from "react";
 import Button from "@mui/material/Button";
 import { Outlet } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 export default function Landing() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    // Clear the history stack when the component mounts
+    navigate("/", { replace: false });
+  }, [navigate]);
   return (
     <div className="loginContainer">
       <Button variant="contained" className="loginButton d-block m-5">
