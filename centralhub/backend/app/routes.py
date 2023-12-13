bcf2962741f31c55605c7ae4e9571407d536d552
N4h33n <naheenkabir@gmail.com>
2023-12-12 21:40:34 -0700

    dedwd

--- centralhub/backend/app/routes.py
+++ centralhub/backend/app/routes.py
@@ -8,13 +8,13 @@ from werkzeug.security import generate_password_hash, check_password_hash
 # reference for using parametrized queries (query, values) to prevent sql injections in flask: https://www.reddit.com/r/flask/comments/zr9148/question_about_protecting_against_sql_injections/
 
 routes = Blueprint('routes', __name__)
-host_url = 'http://localhost:3003'
+host_url = 'http://localhost:3000'
 
 def get_db_connection():
     return mysql.connector.connect(
         host='localhost',
         user='root',
-        password="*PASSworld*123",
+        password="sQlprequelwoohoo7676",
         database='centralhub'
     )
     
