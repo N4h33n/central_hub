19c6c3ea18a72d6528c51fd7499481523b8a4265
Abigia Debebe <Abitibebu123@gmail.com>
2023-12-12 20:44:08 -0700

    ...

--- centralhub/src/App.js
+++ centralhub/src/App.js
@@ -147,10 +147,7 @@ function App() {
               element={<Updateinfo />}
             ></Route>
             <Route path="/filtercourses" element={<Filtercourses />}></Route>
-            <Route
-              path="/discoverecas/:studentID"
-              element={<DiscoverECA />}
-            ></Route>
+            <Route path="/discoverecas" element={<DiscoverECA />}></Route>
           </Route>
         </Routes>
       </BrowserRouter>
